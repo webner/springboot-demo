21d252ee553e46654c040cf8508e3c28cf3d99c5
Wolfgang Ebner <wolfgang.p.ebner@gmail.com>
2021-11-26 11:53:59 +0100

    add spring boot demo application

---
+++ src/main/java/com/viennaairport/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.viennaairport.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("name", "OpenShift");
+        return "hello";
+    }
+}
